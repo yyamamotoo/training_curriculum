83171b6a56c05a5626d81ccaa9d0d430dd3cf8fb
yyamamotoo <71579495+yyamamotoo@users.noreply.github.com>
2020-10-07 20:01:03 +0900

    issue4

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,10 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_04_14_072724) do
-
   create_table "plans", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "plan"
     t.date "date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
-
 end
